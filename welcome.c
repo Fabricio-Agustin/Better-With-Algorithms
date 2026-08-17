6a326dc1cc51063b458e7c8aabb9a042ce021525
Agustin G <agustingibautcarrizo@gmail.com>
2026-08-17 20:32:12 -0300

    Better With Algorithms

---
+++ welcome.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+
+int main() {
+    printf("Bienvenido a Better With Algorithms.\n");
+    printf("Este repositorio nace con un objetivo.\n");
+    printf("Aprender algoritmos y matematicas mediante\n");
+    printf("implementaciones propias en C.\n\n");
+
+    printf("Aqui voy a trabajar con:\n\n");
+    printf("  [1] Algoritmos matematicos\n");
+    printf("  [2] Algoritmos de busqueda\n");
+    printf("  [3] Algoritmos de ordenamiento\n");
+    printf("  [4] Estructuras de datos\n");
+    printf("  [5] Complejidad algoritmica\n");
+    printf("  [6] Problemas y desafios\n\n");
+
+    printf("La idea no es solamente hacer que el codigo\n");
+    printf("funcione, sino entender como funciona.\n\n");
+
+    printf("Cada algoritmo sera una oportunidad para\n");
+    printf("mejorar mi logica, razonamiento y conocimientos\n");
+    printf("de programacion.\n\n");
+
+    printf("\"Talk is cheap. Show me the code.\"\n");
+    printf("                    - Linus Torvalds\n");
+
+    return 0;
+}
